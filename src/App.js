29735edae3eeb2c65d7816dedc1df507f989a9cd
aizenbergivan <aizenbergivan@gmail.com>
2019-10-15 13:54:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there! bla bla bla
         </p>
         <a
           className="App-link"
